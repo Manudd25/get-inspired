77e86916518ce5546763d4581438ba37123e23e5
Manuela Di Dario <didariomanuela@gmail.com>
2025-10-03 13:23:36 +0200

    endpoint adjusted

--- backend/quotes.php
+++ backend/quotes.php
@@ -1,10 +1,16 @@
 <?php
+// CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+// Actual quotes 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: https://happy-bay-016ec1303.1.azurestaticapps.net");
-
 $quotes = json_decode(file_get_contents("quotes.json"), true);
-
-// pick a random quote
 $randomQuote = $quotes[array_rand($quotes)];
-
 echo json_encode($randomQuote);
