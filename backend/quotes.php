5e403c087eebf416056e79a62a27865bc97899a4
Manuela Di Dario <didariomanuela@gmail.com>
2025-10-03 13:17:58 +0200

    CORS modified in quotes.php

--- backend/quotes.php
+++ backend/quotes.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *"); // allow Vue dev server
+header("Access-Control-Allow-Origin: https://happy-bay-016ec1303.1.azurestaticapps.net");
 
 $quotes = json_decode(file_get_contents("quotes.json"), true);
 
